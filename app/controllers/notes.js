c058f31d0adf9ecdaf6093e355e5b2a0e0424704
Steve Kinney <hello@stevekinney.net>
2016-02-22 13:45:21 -0700

    Create a new note in the controller

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  newNote: this.store.createRecord('note'),
+
+  actions: {
+    createNote() {
+      console.log(this.get('newNote.id'));
+      this.set('newNote', this.store.createRecord('note'));
+    }
+  }
+
 });
